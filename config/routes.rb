ec1e03ea847d3467a94356453314bcf5ea4de9f2
disc0ninja <disc0ninja77@gmai.com>
2015-11-07 04:06:48 +0000

    added 'about' page and routes / updated 'home' page / updated css

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # root 'welcome#index'
   root 'pages#home'
   get 'pages/home', to: 'pages#home'
+  get 'pages/about', to: 'pages#about'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
