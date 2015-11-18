10d34e857b05f7cda0fe58777b5ce58ad3f87911
disc0ninja <disc0ninja77@gmai.com>
2015-11-18 02:43:58 +0000

    restricted actions, added login upon signup, updated styling,

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,5 +1,7 @@
 class IdeasController < ApplicationController
   before_action :set_idea, only: [:edit, :update, :show, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   #INDEX
   def index
     @ideas = Idea.all
@@ -8,7 +10,7 @@ class IdeasController < ApplicationController
   #NEW
   def new
     @idea = Idea.new
-    
+
   end
 
   #EDIT
@@ -18,7 +20,7 @@ class IdeasController < ApplicationController
   #CREATE
   def create
     @idea = Idea.new(idea_params)
-    @idea.user = User.first
+    @idea.user = current_user
     if @idea.save
       flash[:success]="Department successfully saved!"
       redirect_to idea_path(@idea)
@@ -55,6 +57,13 @@ class IdeasController < ApplicationController
     def idea_params
       params.require(:idea).permit(:title, :description)
     end
+
+  #define require same
+  def require_same_user
+    if current_user != @idea.user
+      flash[:danger] = "You must be a department owner to do that"
+    end
+  end
 
   def set_idea
     @idea = Idea.find(params[:id])
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
+  before_action :require_same_user, only: [:edit, :update]
    #index
   def index
     @users = User.all
@@ -12,8 +14,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to Brain-Departments #{@user.username}"
-      redirect_to ideas_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
@@ -21,12 +24,12 @@ class UsersController < ApplicationController
 
   #edit
   def edit
-    @user = User.find(params[:id])
+
   end
 
   #update
   def update
-    @user = User.find(params[:id])
+
     if @user.update(user_params)
       flash[:success] = "Your account was updated successfully"
       redirect_to ideas_path
@@ -36,7 +39,18 @@ class UsersController < ApplicationController
   end
   #show
   def show
-    @user = User.find(params[:id])
+
+  end
+
+  def set_user
+     @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = "You must be account owner"
+      redirect_to root_path
+    end
   end
 
 
